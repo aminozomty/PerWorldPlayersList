9f2d0bc52ed5431ff53ef585eda4d10a73b9b729
KygekDev <68368066+Kygekraqmak@users.noreply.github.com>
2020-08-27 22:58:38 +0800

    Added license notice

--- src/aminozomty/PerWorldPlayersList/Main.php
+++ src/aminozomty/PerWorldPlayersList/Main.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * A PocketMine-MP plugin that makes only shows players that are in the same world in the player list menu.
+ * Copyright (C) 2020 aminozomty
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace aminozomty\PerWorldPlayersList;
 
 use pocketmine\plugin\PluginBase;
